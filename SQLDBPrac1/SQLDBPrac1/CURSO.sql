0c4faade5e5efb84429e54e35f1ee64ad2e16604
Luisana Gil <lgil2@udi.edu.co>
2021-09-17 08:16:57 -0500

    Crear tablas

---
+++ SQLDBPrac1/SQLDBPrac1/CURSO.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CURSO]
+(
+	[Cod] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(50) NOT NULL, 
+    [Fecha_inicio] NVARCHAR(50) NOT NULL, 
+    [Duración] INT NOT NULL, 
+    [Valor] INT NOT NULL
+)
